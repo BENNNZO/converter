66caa18ab0c2b563ac6118a56329db88116119c3
Benjamin Phillips <benjamingt537@gmail.com>
2023-07-10 14:17:25 -0400

    got a python script running

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,5 +1,10 @@
 const { exec } = require("child_process")
+const { spawn } = require('child_process')
 
 export async function POST() {
     console.log("POST endpoint")
+    const childPython = spawn('python', ['--version'])
+    childPython.stdout.on('data', (data) => console.log(data.toString('utf8')))
+    // console.log(childPython)
+    return new Response("yay", { status: 200 })
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
 "use client"
 
+import axios from "axios"
 
 export default function Home() {
     function execute() {
         console.log("hello world!")
+        axios.post('/api/test')
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
     }
 
     return (
